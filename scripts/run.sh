b58387cd82a09b44ab4a22e4c182504b1229636c
Steve Griffith <steve.griffith@microsoft.com>
2021-08-03 14:24:59 -0400

    debugging

--- scripts/run.sh
+++ scripts/run.sh
@@ -80,63 +80,63 @@ echo "SSH_KEY_PATH: $SSH_KEY_PATH"
 echo "USER_ASSIGNED_MI_ID: $USER_ASSIGNED_MI_ID"
 echo "------------------------------------------------"
 
-# # Create the Resource Group to deploy the Webinar Environment
-# az group create --name $RG_NAME --location $RG_LOCATION
+# Create the Resource Group to deploy the Webinar Environment
+az group create --name $RG_NAME --location $RG_LOCATION
 
-# # Deploy the jump server and K3s cluster
-# echo "Deploying branch office resources...."
-# az deployment group create \
-#   --name $ARM_DEPLOYMENT_NAME \
-#   --mode Incremental \
-#   --resource-group $RG_NAME \
-#   --template-file ../deploy.bicep \
-#   --parameters prefix=$PREFIX \
-#   --parameters k3sToken="$K3S_TOKEN" \
-#   --parameters adminUsername="$ADMIN_USER_NAME" \
-#   --parameters adminPublicKey="$SSH_PUB_KEY" \
-#   --parameters userAssignedMIID="$USER_ASSIGNED_MI_ID"
+# Deploy the jump server and K3s cluster
+echo "Deploying branch office resources...."
+az deployment group create \
+  --name $ARM_DEPLOYMENT_NAME \
+  --mode Incremental \
+  --resource-group $RG_NAME \
+  --template-file ../deploy.bicep \
+  --parameters prefix=$PREFIX \
+  --parameters k3sToken="$K3S_TOKEN" \
+  --parameters adminUsername="$ADMIN_USER_NAME" \
+  --parameters adminPublicKey="$SSH_PUB_KEY" \
+  --parameters userAssignedMIID="$USER_ASSIGNED_MI_ID"
 
 
-# # Get the jump server public IP
-# JUMP_IP=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.publicIP.value)
-# echo "Jump box connection info: ssh $ADMIN_USER_NAME@$JUMP_IP -i $SSH_KEY_PATH/id_rsa"
+# Get the jump server public IP
+JUMP_IP=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.publicIP.value)
+echo "Jump box connection info: ssh $ADMIN_USER_NAME@$JUMP_IP -i $SSH_KEY_PATH/id_rsa"
 
-# # Get the host name for the control host
-# CONTROL_HOST_NAME=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.controlName.value)
-# echo "Control Host Name: $CONTROL_HOST_NAME"
+# Get the host name for the control host
+CONTROL_HOST_NAME=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.controlName.value)
+echo "Control Host Name: $CONTROL_HOST_NAME"
 
-# # Get the host name for the control host
-# JUMP_VM_NAME=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.jumpVMName.value)
-# echo "Jump Host Name: $JUMP_VM_NAME"
+# Get the host name for the control host
+JUMP_VM_NAME=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.jumpVMName.value)
+echo "Jump Host Name: $JUMP_VM_NAME"
 
-# echo "Wait for jump server to start"
-# while [[ "$(az vm list -d -g $RG_NAME -o tsv --query "[?name=='$JUMP_VM_NAME'].powerState")" != "VM running" ]]
-# do
-# echo "Waiting...."
-#   sleep 5
-# done
-# echo "Jump Server Running!"
+echo "Wait for jump server to start"
+while [[ "$(az vm list -d -g $RG_NAME -o tsv --query "[?name=='$JUMP_VM_NAME'].powerState")" != "VM running" ]]
+do
+echo "Waiting...."
+  sleep 5
+done
+echo "Jump Server Running!"
 
-# # Give the VM a few more seconds to become available
-# sleep 20
+# Give the VM a few more seconds to become available
+sleep 20
 
-# # Copy the private key up to the jump server to be used to access the rest of the nodes
-# echo "Copying private key to jump server..."
-# scp -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP:~/.ssh
+# Copy the private key up to the jump server to be used to access the rest of the nodes
+echo "Copying private key to jump server..."
+scp -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP:~/.ssh
 
-# # Execute setup script on jump server
-# echo "Executing setup script on jump server...."
-# ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP "curl -sfL https://raw.githubusercontent.com/swgriffith/azure-guides/master/temp/get-kube-config.sh |CONTROL_HOST=$CONTROL_HOST_NAME sh -"
+# Execute setup script on jump server
+echo "Executing setup script on jump server...."
+ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP "curl -sfL https://raw.githubusercontent.com/swgriffith/azure-guides/master/temp/get-kube-config.sh |CONTROL_HOST=$CONTROL_HOST_NAME sh -"
 
-# # Get managd identity object id
-# MI_APP_ID=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.userAssignedMIAppID.value)
-# MI_OBJ_ID=$(az ad sp show --id $MI_APP_ID -o tsv --query objectId)
-# echo "User Assigned Managed Identity App ID: $MI_APP_ID"
-# echo "User Assigned Managed Identity Object ID: $MI_OBJ_ID"
+# Get managd identity object id
+MI_APP_ID=$(az deployment group show -g $RG_NAME -n $ARM_DEPLOYMENT_NAME -o tsv --query properties.outputs.userAssignedMIAppID.value)
+MI_OBJ_ID=$(az ad sp show --id $MI_APP_ID -o tsv --query objectId)
+echo "User Assigned Managed Identity App ID: $MI_APP_ID"
+echo "User Assigned Managed Identity Object ID: $MI_OBJ_ID"
 
-# # Arc join the cluster
-# echo "Arc joining the branch cluster..."
-#ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP "az connectedk8s connect -g $RG_NAME -n $PREFIX-branch --distribution k3s --infrastructure generic --custom-locations-oid $MI_OBJ_ID"
+# Arc join the cluster
+echo "Arc joining the branch cluster..."
+ssh -o "StrictHostKeyChecking no" -i $SSH_KEY_PATH/id_rsa $ADMIN_USER_NAME@$JUMP_IP "az connectedk8s connect -g $RG_NAME -n $PREFIX-branch --distribution k3s --infrastructure generic --custom-locations-oid $MI_OBJ_ID"
 }
 
 # Execute Functions
